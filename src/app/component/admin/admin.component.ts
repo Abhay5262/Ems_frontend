8d3c4a6a54170378654ca9c860383d23652fc1d6
Abhay5262 <abhayjoshi.ce20@sltiet.com>
2024-04-30 16:03:23 +0530

    animation

--- src/app/component/admin/admin.component.ts
+++ src/app/component/admin/admin.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { DataproviderService } from 'src/app/service/dataprovider.service';
 import Swal from 'sweetalert2';
 import { SinglestudentComponent } from '../singlestudent/singlestudent.component';
 import { MatDialog } from '@angular/material/dialog';
 import { UpdateemployeComponent } from 'src/app/updateemploye/updateemploye.component';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  animations: [
+    // trigger('buttonAnimation', [
+    //   transition(':enter', [
+    //     style({ opacity: 5, transform:'scale(0.4) rotate(20deg)' }),
+    //     animate('2000ms cubic-bezier(0.68, 0.55, 0.27, 1.55)' , style({ opacity: 1, transform: 'translateY(0)' })),
+    //   ]),
+    // ]),
+    trigger('buttonAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'scale(0.9)' }), // Initial state: slightly scaled down
+        animate('1000ms cubic-bezier(0.215, 0.61, 0.355, 1)', style({ opacity: 1, transform: 'scale(1)' })), // Smooth fade-in and scale-up
+      ]),
+    ]),
+    trigger('buttonHoverAnimation', [
+      transition(':enter', [
+        style({ transform: 'scale(1)' }),
+        animate('200ms', style({ transform: 'scale(1.1)' })),
+      ]),
+      transition(':leave', [
+        animate('200ms', style({ transform: 'scale(1)' })),
+      ]),
+    ]),
+  
+  ],
 })
 export class AdminComponent {
   students:any;
@@ -71,4 +96,16 @@ export class AdminComponent {
         this.getall()
       });
     }
-}
+
+    isHovered: boolean = false;
+
+    @HostListener('mouseenter')
+    onMouseEnter() {
+      this.isHovered = true;
+    }
+  
+    @HostListener('mouseleave')
+    onMouseLeave() {
+      this.isHovered = false;
+    }
+  }
